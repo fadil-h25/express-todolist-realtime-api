ab4236c08dba6b041152a79ed183a93827cb683e
fadil-coder <fadilhambali25@gmail.com>
2025-08-25 10:56:08 +0800

    feat(main): restructure main.ts to include middleware and improve request handling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import express from "express";
+import express, { type Request } from "express";
+import { globalMiddleware } from "./middleware/global/index.js";
+import { logger } from "./logger/index.js";
+import { generateLogMetaData } from "./helper/generate-log-meta-data.js";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Aplly global middlewares
+globalMiddleware.forEach((middleware) => app.use(middleware));
+
+app.get("/test", (req: Request, res) => {
+  logger.info(
+    "request received",
+    generateLogMetaData(req.requestId as string, req.route.path, "main")
+  );
   res.send("Hello World!");
 });
 
