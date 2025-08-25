7f79b8e6a85c997f74811102d5a19909e6379349
fadil-coder <fadilhambali25@gmail.com>
2025-08-25 19:04:23 +0800

    feat(database): add setup for prisma

---
+++ src/database/index.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+import { logger } from "../logger/index.js";
+
+const prisma = new PrismaClient();
+
+export async function checkDatabaseConnection() {
+  try {
+    await prisma.$connect();
+    logger.info("Database connected successfully");
+  } catch (error) {
+    logger.error("Database connection error", { error });
+    process.exit(1);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import express, { type Request } from "express";
+import express from "express";
+import { Request, Response } from "express";
 import { globalMiddleware } from "./middleware/global/index.js";
 import { logger } from "./logger/index.js";
 import { generateLogMetaData } from "./helper/generate-log-meta-data.js";
+import dotenv from "dotenv";
+import { checkDatabaseConnection } from "./database/index.js";
+
+dotenv.config();
 
 const app = express();
+checkDatabaseConnection();
 const port = 3000;
 
 app.use(express.json());
@@ -12,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 // Aplly global middlewares
 globalMiddleware.forEach((middleware) => app.use(middleware));
 
-app.get("/test", (req: Request, res) => {
+app.get("/test", (req: Request, res: Response) => {
   logger.info(
     "request received",
     generateLogMetaData(req.requestId as string, req.route.path, "main")
