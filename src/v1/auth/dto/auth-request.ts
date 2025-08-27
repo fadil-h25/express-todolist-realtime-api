d574ce0e863810b53b75476d513a6d84be7372fa
fadil-coder <fadilhambali25@gmail.com>
2025-08-27 15:50:16 +0800

    feat(auth): update authentication schemas to use consistent naming and add registration schema

--- src/v1/auth/dto/auth-request.ts
+++ src/v1/auth/dto/auth-request.ts
@@ -1,4 +1,5 @@
 import z from "zod";
-import { authLoginSchema } from "../schema/auth-schema";
+import { AuthLoginSchema, AuthRegisterSchema } from "../schema/auth-schema";
 
-export type AuthLoginRequest = z.infer<typeof authLoginSchema>;
+export type AuthLoginRequest = z.infer<typeof AuthLoginSchema>;
+export type AuthRegisterRequest = z.infer<typeof AuthRegisterSchema>;
--- src/v1/auth/schema/auth-schema.ts
+++ src/v1/auth/schema/auth-schema.ts
@@ -1,10 +1,19 @@
 import z from "zod";
 import {
   userEmailSchema,
+  userNameSchema,
   userPasswordSchema,
+  userRoleSchema,
 } from "../../user/schema/user-schema";
 
-export const authLoginSchema = z.object({
+export const AuthLoginSchema = z.object({
   email: userEmailSchema,
   password: userPasswordSchema,
+});
+
+export const AuthRegisterSchema = z.object({
+  email: userEmailSchema,
+  password: userPasswordSchema,
+  name: userNameSchema,
+  role: userRoleSchema.optional().default("USER"),
 });
