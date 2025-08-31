34a6fc32fef7a7ae4a7c81326219fd7608c608ca
fadil-coder <fadilhambali25@gmail.com>
2025-08-31 21:58:41 +0800

    chore(middleware-global): add  debug log

--- src/middleware/global/reqeust-context-middleware.ts
+++ src/middleware/global/reqeust-context-middleware.ts
@@ -1,11 +1,13 @@
 import type { NextFunction, Request, Response } from "express";
 import { v4 } from "uuid";
+import { logger } from "../../logger";
 
 export const RequestContextMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  logger.debug("RequestContextMiddleware() is running");
   req.context = {
     reqId: v4(),
     route: req.path,
