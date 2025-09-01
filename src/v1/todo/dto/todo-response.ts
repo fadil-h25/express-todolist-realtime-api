7a5c8b435cb24e517fbe7b0ba5c29fa9fe56cc42
fadil-coder <fadilhambali25@gmail.com>
2025-09-02 06:43:47 +0800

    fix(todo): align response with latest Todo model

--- src/v1/todo/dto/todo-response.ts
+++ src/v1/todo/dto/todo-response.ts
@@ -1,9 +1,22 @@
+import { TodoStatus } from "@prisma/client";
+
 export type TodoResponse = {
   id: string;
   title: string;
-  isPublic: boolean;
-  status: "PENDING" | "PROGRESS" | "COMPLETED";
-  ownerId: string;
+  status: TodoStatus;
+  todolistId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type TodoResponseDetails = {
+  id: string;
+  todolistId: string;
+  title: string;
+  description: string | null;
+
+  status: TodoStatus;
+
   createdAt: Date;
   updatedAt: Date;
 };
