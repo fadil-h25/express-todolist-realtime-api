7284fe994758d8c654dc95a4e3e42eb84e2a3d82
fadil-coder <fadilhambali25@gmail.com>
2025-09-15 01:12:39 +0800

    feat(middleware): handle PrismaClientKnownRequestError

--- src/middleware/error-handler-middleware.ts
+++ src/middleware/error-handler-middleware.ts
@@ -4,6 +4,7 @@ import { ResponseErrorBody } from "../types/response/response-error-body.js";
 import { logger } from "../logger/index.js";
 import { ZodError } from "zod";
 import { ZodErrorMapper } from "../helper/zod-error-mapper.js";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library.js";
 
 export const ErrorHandlerMiddleware = (
   err: unknown,
@@ -26,6 +27,43 @@ export const ErrorHandlerMiddleware = (
       errors: ZodErrorMapper(err),
     };
     return res.status(400).json(responseErrorBody);
+  } else if (err instanceof PrismaClientKnownRequestError) {
+    logger.debug(`Error catch PrismaClientKnownRequestError `, err);
+    let responseErrorBody: ResponseErrorBody | null;
+    let message: string | null;
+
+    switch (err.code) {
+      case "P2025": // Record not found
+        const modelName = err.meta?.modelName || "Record";
+        responseErrorBody = {
+          message: `${modelName} not found`,
+          success: false,
+        };
+        return res.status(404).json(responseErrorBody);
+
+      case "P2002": // Unique constraint failed
+        const targetField = err.meta?.target || "field";
+        responseErrorBody = {
+          message: `Duplicate value for ${targetField}`,
+          success: false,
+        };
+        return res.status(400).json(responseErrorBody);
+
+      case "P2003": // Foreign key constraint failed
+        responseErrorBody = {
+          message: `Cannot perform operation due to related records`,
+          success: false,
+        };
+        return res.status(400).json(responseErrorBody);
+
+      default:
+        // Prisma error lain
+        responseErrorBody = {
+          message: `Prisma error: ${err.message}`,
+          success: false,
+        };
+        return res.status(500).json(responseErrorBody);
+    }
   } else {
     logger.error(`Unhandled Error: ${err}`);
     return res.status(500).json({
