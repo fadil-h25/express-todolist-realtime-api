d8767be2d893557a7bdfc04453217ba731e298e6
fadil-coder <fadilhambali25@gmail.com>
2025-09-15 01:14:55 +0800

    fix(auth): import config load-env

--- src/middleware/auth-request-middleware.ts
+++ src/middleware/auth-request-middleware.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { CustomError } from "../error/CustomError.js";
 import { logger } from "../logger/index.js";
-
+import "../config/load-env.js";
 export const AuthRequestiddleware = (
   req: Request,
   res: Response,
@@ -12,14 +12,16 @@ export const AuthRequestiddleware = (
   try {
     logger.debug("AuthRequestMiddleware() is running");
     const accessToken = req.cookies["access_token"];
+    if (!accessToken) throw new CustomError("Invalid token", 401);
 
-    const secretKey = process.env.SECRET_KEY;
-    if (!secretKey) throw new CustomError("Secret key is null", 500);
+    const secretKey = process.env.JWT_SECRET_KEY;
+    if (!secretKey) throw new Error("Secret key is null");
 
     const payload = jwt.verify(accessToken, secretKey, {});
     if (!payload) throw new CustomError("Invalid token", 401);
     req.context.userId = (payload as any).userId;
     req.context.role = (payload as any).role;
+    next();
   } catch (error) {
     next(error);
   }
