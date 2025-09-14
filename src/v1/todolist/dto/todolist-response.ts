d477b9a2a83ac8e37c78bbad95c33fcfb1a4de93
fadil-coder <fadilhambali25@gmail.com>
2025-09-14 14:54:29 +0800

    feat(todolist): add TodolistResponseDetails type

--- src/v1/todolist/dto/todolist-response.ts
+++ src/v1/todolist/dto/todolist-response.ts
@@ -1,3 +1,5 @@
+import { TodoResponse } from "../../todo/dto/todo-response";
+
 export type TodolistResponse = {
   id: string;
   title: string;
@@ -6,4 +8,15 @@ export type TodolistResponse = {
   isPublic: boolean;
   createdAt: Date;
   updatedAt: Date;
+};
+
+export type TodolistResponseDetails = {
+  id: string;
+  title: string;
+  description: string | null;
+  ownerId: string;
+  isPublic: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  todos: TodoResponse[];
 };
