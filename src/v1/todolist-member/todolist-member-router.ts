a01e94dc2a66931ca1db3e073879cfdacc8c8bdd
fadil-coder <fadilhambali25@gmail.com>
2025-09-16 15:29:20 +0800

    refactor(todolist-member): enable mergeParams in router

--- src/v1/todolist-member/todolist-member-router.ts
+++ src/v1/todolist-member/todolist-member-router.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { handleCreateTodolistMember } from "./todolist-member-controller.js";
 
-export const todolistMemberRouter = express.Router();
+export const todolistMemberRouter = express.Router({ mergeParams: true });
 
 todolistMemberRouter.post("", handleCreateTodolistMember);
