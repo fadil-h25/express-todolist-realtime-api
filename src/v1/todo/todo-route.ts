01da33598b015548cb14ffdf762f5b5296ec1d4c
fadil-coder <fadilhambali25@gmail.com>
2025-09-16 11:50:27 +0800

    feat(todo): add route for delete todo

--- src/v1/todo/todo-route.ts
+++ src/v1/todo/todo-route.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import {
   handleCreateTodo,
-  handleDeletodoById,
+  handleDeleteTodoById,
   handleGetTodoById,
   handleGetTodos,
   handleUpdateTodoById,
 } from "./todo-controller.js";
 
-export const todoRouter = express.Router();
+export const todoRouter = express.Router({ mergeParams: true });
 
 todoRouter.get("", handleGetTodos);
 todoRouter.post("", handleCreateTodo);
 todoRouter.get("/:id", handleGetTodoById);
 
 todoRouter.put("/:id", handleUpdateTodoById);
-todoRouter.delete("/:id", handleDeletodoById);
+todoRouter.delete("/:id", handleDeleteTodoById);
