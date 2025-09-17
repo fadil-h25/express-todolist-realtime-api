38626ae5b64b000320955021f766eb034556e277
fadil-coder <fadilhambali25@gmail.com>
2025-09-17 10:34:51 +0800

    feat(helper): add zod error mapper

--- src/helper/zod-error-mapper.ts
+++ src/helper/zod-error-mapper.ts
@@ -1,7 +1,9 @@
 import { ZodError } from "zod";
-import { FormatZodError } from "../types/format-zod-error";
+import { FormatZodError } from "../types/format-zod-error.js";
+import { logger } from "../logger/index.js";
 
 export function ZodErrorMapper(error: ZodError): FormatZodError[] {
+  logger.debug("zodErrorMapper running()");
   const formatError = error!.issues.map((issue) => {
     return {
       path: issue.path as string[],
