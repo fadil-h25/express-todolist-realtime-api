da662e7c2fe55cefb42a669c8254640edd788cbb
fadil-coder <fadilhambali25@gmail.com>
2025-09-18 23:08:35 +0800

    feat(todo): add request type GetTodosRequest

--- src/v1/todo/dto/todo-request.ts
+++ src/v1/todo/dto/todo-request.ts
@@ -3,6 +3,7 @@ import {
   CreateTodoSchema,
   DeleteTodoByIdSchema,
   GetTodoByIdSchema,
+  GetTodosSchema,
   UpdateTodoSchema,
 } from "../schema/todo-schema.js";
 
@@ -10,3 +11,4 @@ export type CreateTodoRequest = z.infer<typeof CreateTodoSchema>;
 export type UpdateTodoRequest = z.infer<typeof UpdateTodoSchema>;
 export type GetTodoByIdRequest = z.infer<typeof GetTodoByIdSchema>;
 export type DeleteTodoByIdRequest = z.infer<typeof DeleteTodoByIdSchema>;
+export type GetTodosRequest = z.infer<typeof GetTodosSchema>;
