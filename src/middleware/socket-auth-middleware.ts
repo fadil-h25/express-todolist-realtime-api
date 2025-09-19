ee40b6a62d1c553cef5a01e4d20614cb82652f7d
fadil-coder <fadilhambali25@gmail.com>
2025-09-20 02:01:10 +0800

    feat(middleware): add cookie parse for socket auth

--- src/middleware/socket-auth-middleware.ts
+++ src/middleware/socket-auth-middleware.ts
@@ -1,17 +1,25 @@
+import cookie from "cookie";
 import { Socket } from "socket.io";
 import jwt from "jsonwebtoken";
-import { CustomError } from "../error/CustomError";
-import { logger } from "../logger";
-import { SocketContext } from "../types/context";
+import { CustomError } from "../error/CustomError.js";
+import { logger } from "../logger/index.js";
+import { SocketContext } from "../types/context.js";
 
 export function socketAuthMiddlewre(
   socket: Socket,
   next: (err?: Error) => void
 ) {
   try {
-    logger.debug("SocketAutMiddleware() is running");
-    const accessToken = socket.handshake.auth.accessToken;
-    if (!accessToken) throw new CustomError("Invalid token", 401);
+    logger.debug("SocketAuthMiddleware() is running");
+
+    const rawCookies = socket.handshake.headers.cookie;
+
+    if (!rawCookies) throw new CustomError("Token invalid", 401);
+
+    const cookies = cookie.parse(rawCookies);
+    const accessToken = cookies["access_token"];
+
+    if (!accessToken) throw new CustomError("Token invalid", 401);
 
     const secretKey = process.env.JWT_SECRET_KEY;
     if (!secretKey) throw new Error("Secret key is null");
@@ -19,6 +27,9 @@ export function socketAuthMiddlewre(
     const payload = jwt.verify(accessToken, secretKey, {});
     (socket.data.context as SocketContext).userId = (payload as any).userId;
     (socket.data.context as SocketContext).role = (payload as any).role;
+
     next();
-  } catch (error) {}
+  } catch (error) {
+    next(new CustomError("Unauthorized", 401));
+  }
 }
